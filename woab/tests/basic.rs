8709ff91c0fad4d96ef441e07fac7f01a0285cb2
Idan Arye <IdanArye@gmail.com>
2020-08-25 17:31:09 +0300

    Add test

---
+++ woab/tests/basic.rs
@@ -0,0 +1,82 @@
+use gtk::prelude::*;
+
+#[macro_use]
+mod util;
+
+
+#[derive(woab::Factories)]
+struct Factories {
+    #[factory(extra(buf_left, buf_right))]
+    win_test: woab::ActorFactory<TestActor>,
+}
+
+struct TestActor {
+    widgets: TestWidgets,
+}
+
+impl actix::Actor for TestActor {
+    type Context = actix::Context<Self>;
+}
+
+impl woab::WoabActor for TestActor {
+    type Widgets = TestWidgets;
+    type Signal = TestSignal;
+}
+
+#[derive(Clone, woab::WidgetsFromBuilder)]
+pub struct TestWidgets {
+    win_test: gtk::ApplicationWindow,
+    btn_copy_right_to_left: gtk::Button,
+    btn_copy_left_to_right: gtk::Button,
+    buf_left: gtk::TextBuffer,
+    buf_right: gtk::TextBuffer,
+}
+
+#[derive(woab::BuilderSignal)]
+enum TestSignal {
+    CopyRightToLeft(gtk::Button),
+    CopyLeftToRight,
+}
+
+fn get_text(buffer: &gtk::TextBuffer) -> String {
+    if let Some(text) = buffer.get_text(&buffer.get_start_iter(), &buffer.get_end_iter(), true) {
+        text.into()
+    } else {
+        "".to_owned()
+    }
+}
+
+impl actix::StreamHandler<TestSignal> for TestActor {
+    fn handle(&mut self, signal: TestSignal, _ctx: &mut Self::Context) {
+        match signal {
+            TestSignal::CopyRightToLeft(_) => {
+                self.widgets.buf_left.set_text(&get_text(&self.widgets.buf_right));
+            },
+            TestSignal::CopyLeftToRight => {
+                self.widgets.buf_right.set_text(&get_text(&self.widgets.buf_left));
+            },
+        }
+    }
+}
+
+#[test]
+fn test_basic() -> anyhow::Result<()> {
+    let factories = Factories::read(include_bytes!("basic.glade") as &[u8])?;
+    gtk::init()?;
+    woab::run_actix_inside_gtk_event_loop("test")?;
+    let mut put_widgets_in = None;
+    factories.win_test.create(|_, widgets| {
+        put_widgets_in = Some(widgets.clone());
+        TestActor { widgets }
+    })?;
+    let widgets = put_widgets_in.unwrap();
+    widgets.buf_left.set_text("test left");
+    wait_for!(get_text(&widgets.buf_right) == "")?;
+    widgets.btn_copy_left_to_right.emit_clicked();
+    wait_for!(get_text(&widgets.buf_right) == "test left")?;
+    widgets.buf_left.set_text("");
+    widgets.buf_right.set_text("test right");
+    widgets.btn_copy_right_to_left.emit_clicked();
+    wait_for!(get_text(&widgets.buf_left) == "test right")?;
+    Ok(())
+}
---
+++ woab/tests/util.rs
@@ -0,0 +1,16 @@
+#[macro_export]
+macro_rules! wait_for {
+    ($pred:expr) => {{
+        let timeout = std::time::Duration::from_secs(1);
+        let time_limit = std::time::Instant::now() + timeout;
+        loop {
+            let is_over = time_limit < std::time::Instant::now();
+            if $pred {
+                break Ok(());
+            } else if is_over {
+                break Err(anyhow::Error::msg(concat!("Timed out: ", stringify!($pred))));
+            }
+            gtk_test::wait(10);
+        }
+    }};
+}
