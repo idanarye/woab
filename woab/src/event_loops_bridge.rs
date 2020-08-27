c8f353fc63a1dff5eea99c260532c586afdeb493
Idan Arye <IdanArye@gmail.com>
2020-08-27 16:15:05 +0300

    Use `glib::idle_add_local` instead of `gtk::idle_add`

--- woab/src/event_loops_bridge.rs
+++ woab/src/event_loops_bridge.rs
@@ -3,7 +3,7 @@ pub fn run_actix_inside_gtk_event_loop(name: impl Into<String>) -> std::io::Resu
     let mut runtime = tokio::runtime::Runtime::new()?;
     let local_set = tokio::task::LocalSet::new();
     local_set.spawn_local(actix::System::run_in_tokio(name.into(), &local_set));
-    gtk::idle_add(move || {
+    glib::idle_add_local(move || {
         local_set.block_on(&mut runtime, async {
             tokio::time::delay_for(tokio::time::Duration::new(0, 0)).await;
         });
