d4eec5c94b97750f5a54573dcb447f094ac536db
IdanArye <idanarye@gmail.com>
2021-02-12 04:24:26 +0200

    Make `tests/connect_nonbuilder_signals.rs` use actions

--- tests/connect_nonbuilder_signals.rs
+++ tests/connect_nonbuilder_signals.rs
@@ -1,83 +1,96 @@
 use std::rc::Rc;
 use std::cell::RefCell;
+use std::collections::HashMap;
 
-// use actix::prelude::*;
+use actix::prelude::*;
 use gtk::prelude::*;
+use gio::prelude::*;
 
 #[macro_use]
 mod util;
 
-#[derive(woab::Factories)]
-struct Factories {
-    win_test: woab::BuilderFactory,
-}
-
-#[derive(Clone, woab::WidgetsFromBuilder)]
-struct TestWidgets {
-    win_test: gtk::ApplicationWindow,
-    button1: gtk::Button,
-    button2: gtk::Button,
-}
-
 #[derive(Debug, PartialEq, woab::BuilderSignal)]
 enum TestSignal {
-    Click1,
-    Click2,
+    Action1,
+    Action2,
+    BlockAction(gio::SimpleAction, glib::Variant),
 }
 
 struct TestActor {
-    name: &'static str,
-    output: Rc<RefCell<Vec<(&'static str, TestSignal)>>>,
+    action_group: gio::SimpleActionGroup,
+    output: Rc<RefCell<Vec<&'static str>>>,
+    actions: HashMap<&'static str, (gio::SimpleAction, glib::signal::SignalHandlerId)>,
 }
 
 impl actix::Actor for TestActor {
     type Context = actix::Context<Self>;
+
+    fn started(&mut self, ctx: &mut Self::Context) {
+        for (action_name, signal_name) in &[
+            ("action1", "Action1"),
+            ("action2", "Action2"),
+        ] {
+            let action = gio::SimpleAction::new(action_name, None);
+            self.action_group.add_action(&action);
+            self.actions.insert(action_name, (
+                    action.clone(),
+                    woab::connect_signal_handler::<TestActor, TestSignal, _>(&action, "activate", signal_name, ctx).unwrap(),
+            ));
+        }
+
+        let block_action = gio::SimpleAction::new("block", Some(&*String::static_variant_type()));
+        self.action_group.add_action(&block_action);
+        woab::connect_signal_handler::<TestActor, TestSignal, _>(&block_action, "activate", "BlockAction", ctx).unwrap();
+
+        self.output.borrow_mut().push("init");
+    }
 }
 
 impl actix::StreamHandler<TestSignal> for TestActor {
     fn handle(&mut self, signal: TestSignal, _ctx: &mut Self::Context) {
-        self.output.borrow_mut().push((self.name, signal));
+        match signal {
+            TestSignal::Action1 => {
+                self.output.borrow_mut().push("action1");
+            },
+            TestSignal::Action2 => {
+                self.output.borrow_mut().push("action2");
+            },
+            TestSignal::BlockAction(_, action) => {
+                let action = action.get::<String>().unwrap();
+                let (action, signal) = self.actions.remove(&*action).unwrap();
+                action.block_signal(&signal);
+                self.output.borrow_mut().push("block");
+            }
+        }
     }
 }
 
 #[test]
 fn test_connect_nonbuilder_signals() -> anyhow::Result<()> {
-    let factories = Factories::read(include_bytes!("connect_nonbuilder_signals.glade") as &[u8])?;
     gtk::init()?;
     woab::run_actix_inside_gtk_event_loop("test")?;
 
-    let builder = factories.win_test.instantiate();
-    let widgets: TestWidgets = builder.widgets().unwrap();
-
     let output = Rc::new(RefCell::new(Vec::new()));
 
-    builder.actor().create(|ctx| {
-        woab::connect_signal_handler::<TestActor, TestSignal, _>(&widgets.button1, "clicked", "Click1", ctx);
-        TestActor {
-            name: "actor1",
-            output: output.clone(),
-        }
-    });
+    let action_group = gio::SimpleActionGroup::new();
+    TestActor {
+        action_group: action_group.clone(),
+        output: output.clone(),
+        actions: Default::default(),
+    }.start();
 
-    builder.actor().create(|ctx| {
-        woab::connect_signal_handler::<TestActor, TestSignal, _>(&widgets.button2, "clicked", "Click2", ctx);
-        TestActor {
-            name: "actor2",
-            output: output.clone(),
-        }
-    });
+    wait_for!(*output.borrow() == ["init"])?;
+    action_group.activate_action("action1", None);
+    wait_for!(*output.borrow() == ["init", "action1"])?;
+    action_group.activate_action("action2", None);
+    wait_for!(*output.borrow() == ["init", "action1", "action2"])?;
+    action_group.activate_action("block", Some(&"action1".to_variant()));
+    wait_for!(*output.borrow() == ["init", "action1", "action2", "block"])?;
 
-    builder.finish();
-
-    widgets.button1.emit_clicked();
-    wait_for!(*output.borrow() == [
-        ("actor1", TestSignal::Click1),
-    ])?;
-    widgets.button2.emit_clicked();
-    wait_for!(*output.borrow() == [
-        ("actor1", TestSignal::Click1),
-        ("actor2", TestSignal::Click2),
-    ])?;
+    // We send both action1 and action2, but action1 is blocked
+    action_group.activate_action("action1", None);
+    action_group.activate_action("action2", None);
+    wait_for!(*output.borrow() == ["init", "action1", "action2", "block", "action2"])?;
 
     Ok(())
 }
