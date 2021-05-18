ad19473206009418e79d7a8773408077a5ec6e23
Idan Arye <IdanArye@gmail.com>
2021-05-18 19:36:03 +0300

    Add nesting to `test_prop_sync`

--- tests/prop_sync.rs
+++ tests/prop_sync.rs
@@ -2,11 +2,17 @@ use gtk::prelude::*;
 
 #[derive(woab::WidgetsFromBuilder, woab::PropSync)]
 struct TestWidgets {
-    // win_test: gtk::ApplicationWindow,
     #[prop_sync(set, get)]
     text_entry: gtk::Entry,
     #[prop_sync("value": f64, set, get)]
     spin_button: gtk::SpinButton,
+    #[prop_sync(set, get)]
+    #[widget(nested)]
+    inner: InnerWidgets,
+}
+
+#[derive(woab::WidgetsFromBuilder, woab::PropSync)]
+struct InnerWidgets {
     #[prop_sync("active": bool, set, get)]
     check_button: gtk::CheckButton,
 }
@@ -22,12 +28,12 @@ fn test_prop_sync() -> anyhow::Result<()> {
 
     widgets.text_entry.set_text("one");
     widgets.spin_button.set_value(2.0);
-    widgets.check_button.set_active(false);
+    widgets.inner.check_button.set_active(false);
 
     let TestWidgetsPropGetter {
         text_entry,
         spin_button,
-        check_button,
+        inner: InnerWidgetsPropGetter { check_button },
     } = widgets.get_props();
 
     assert_eq!(text_entry, "one");
@@ -37,12 +43,12 @@ fn test_prop_sync() -> anyhow::Result<()> {
     widgets.set_props(&TestWidgetsPropSetter {
         text_entry: "three",
         spin_button: 4.0,
-        check_button: true,
+        inner: InnerWidgetsPropSetter { check_button: true },
     });
 
     assert_eq!(widgets.text_entry.get_text(), "three");
     assert_eq!(widgets.spin_button.get_value(), 4.0f64);
-    assert!(widgets.check_button.get_active());
+    assert!(widgets.inner.check_button.get_active());
 
     Ok(())
 }
