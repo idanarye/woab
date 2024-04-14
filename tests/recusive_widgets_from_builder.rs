463c81714dfc210840846cfebfde6654d767aac0
Idan Arye <IdanArye@gmail.com>
2024-04-14 22:46:45 +0300

    Do the `four_texts` test

--- tests/recusive_widgets_from_builder.rs
+++ tests/recusive_widgets_from_builder.rs
@@ -33,34 +33,33 @@ pub struct GroupB {
 
 #[test]
 fn test_recusive_widgets_from_builder() -> anyhow::Result<()> {
-    let factory = woab::BuilderFactory::from(include_str!("four_texts.glade").to_owned());
-    gtk4::init()?;
-    woab::run_actix_inside_gtk_event_loop();
-    let bld = factory.instantiate();
+    util::test_main(async {
+        let factory = woab::BuilderFactory::from(include_str!("four_texts.ui").to_owned());
+        let bld = factory.instantiate();
 
-    let flat_widgets: FlatWidgets = bld.widgets()?;
-    let grouped_widgets: GroupedWidgets = bld.widgets()?;
+        let flat_widgets: FlatWidgets = bld.widgets()?;
+        let grouped_widgets: GroupedWidgets = bld.widgets()?;
 
-    assert!(grouped_widgets.group_a.text1.text().is_empty());
-    assert!(grouped_widgets.group_a.text2.text().is_empty());
-    assert!(grouped_widgets.group_b.text3.text().is_empty());
-    assert!(grouped_widgets.group_b.text4.text().is_empty());
+        assert!(grouped_widgets.group_a.text1.text().is_empty());
+        assert!(grouped_widgets.group_a.text2.text().is_empty());
+        assert!(grouped_widgets.group_b.text3.text().is_empty());
+        assert!(grouped_widgets.group_b.text4.text().is_empty());
 
-    flat_widgets.text1.set_text("Text 1");
-    flat_widgets.text2.set_text("Text 2");
-    flat_widgets.text3.set_text("Text 3");
-    flat_widgets.text4.set_text("Text 4");
+        flat_widgets.text1.set_text("Text 1");
+        flat_widgets.text2.set_text("Text 2");
+        flat_widgets.text3.set_text("Text 3");
+        flat_widgets.text4.set_text("Text 4");
 
-    wait_for!(flat_widgets.text1.text() == "Text 1")?;
-    wait_for!(flat_widgets.text2.text() == "Text 2")?;
-    wait_for!(flat_widgets.text3.text() == "Text 3")?;
-    wait_for!(flat_widgets.text4.text() == "Text 4")?;
+        wait_for!(flat_widgets.text1.text() == "Text 1")?;
+        wait_for!(flat_widgets.text2.text() == "Text 2")?;
+        wait_for!(flat_widgets.text3.text() == "Text 3")?;
+        wait_for!(flat_widgets.text4.text() == "Text 4")?;
 
-    assert!(grouped_widgets.group_a.text1.text() == "Text 1");
-    assert!(grouped_widgets.group_a.text2.text() == "Text 2");
-    assert!(grouped_widgets.group_b.text3.text() == "Text 3");
-    assert!(grouped_widgets.group_b.text4.text() == "Text 4");
+        assert!(grouped_widgets.group_a.text1.text() == "Text 1");
+        assert!(grouped_widgets.group_a.text2.text() == "Text 2");
+        assert!(grouped_widgets.group_b.text3.text() == "Text 3");
+        assert!(grouped_widgets.group_b.text4.text() == "Text 4");
 
-    woab::close_actix_runtime()??;
-    Ok(())
+        Ok(())
+    })
 }
